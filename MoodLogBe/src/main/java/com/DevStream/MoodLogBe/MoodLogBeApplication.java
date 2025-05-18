48bf8b49dd0061f5c9e0491ade4ee1c5b7962e65
forwoni <ktion0619@naver.com>
2025-05-18 23:32:14 +0900

    chore: initial project skeleton (React, Spring Boot, AI service)

---
+++ MoodLogBe/src/main/java/com/DevStream/MoodLogBe/MoodLogBeApplication.java
@@ -0,0 +1,13 @@
+package com.DevStream.MoodLogBe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodLogBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodLogBeApplication.class, args);
+	}
+
+}
