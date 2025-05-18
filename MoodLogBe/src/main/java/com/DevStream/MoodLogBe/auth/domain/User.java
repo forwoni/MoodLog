17149899703d30f0a890c9ced378cfca7a8d8160
forwoni <ktion0619@naver.com>
2025-05-19 02:09:04 +0900

    feat(auth): define User entity and roles collection

--- MoodLogBe/src/main/java/com/DevStream/MoodLogBe/auth/domain/User.java
+++ MoodLogBe/src/main/java/com/DevStream/MoodLogBe/auth/domain/User.java
@@ -1,19 +1,40 @@
 package com.DevStream.MoodLogBe.auth.domain;
 
 import com.DevStream.MoodLogBe.post.domain.Post;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
+
 public class User {
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
     private String username;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    private Set<String> roles = new HashSet<>();
 
     private List<Post> posts;
 }
