753f854680a1d3b820ce86c104037c82e94bc089
forwoni <ktion0619@naver.com>
2025-05-19 16:59:05 +0900

    fix(auth): rename Authcontroller to AuthController

--- MoodLogBe/src/main/java/com/DevStream/MoodLogBe/auth/controller/AuthController.java
+++ MoodLogBe/src/main/java/com/DevStream/MoodLogBe/auth/controller/AuthController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
-public class Authcontroller {
+public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/signup")
