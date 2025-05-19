372bb26e3e0a4ce274a1480460cf7ac71740676f
forwoni <ktion0619@naver.com>
2025-05-19 23:42:49 +0900

    fix(security): add missing imports in SecurityConfig

--- MoodLogBe/src/main/java/com/DevStream/MoodLogBe/config/SecurityConfig.java
+++ MoodLogBe/src/main/java/com/DevStream/MoodLogBe/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.DevStream.MoodLogBe.config;
 
+import com.DevStream.MoodLogBe.auth.filter.JwtAuthenticationFilter;
 import com.DevStream.MoodLogBe.auth.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
