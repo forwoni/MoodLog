b56173153a8923be11a174e5d62743bfc1edd7bd
forwoni <ktion0619@naver.com>
2025-05-21 00:19:20 +0900

    feat(post): post entity 필드 값 설정 및 수정

--- MoodLogBe/src/main/java/com/DevStream/MoodLogBe/post/domain/Post.java
+++ MoodLogBe/src/main/java/com/DevStream/MoodLogBe/post/domain/Post.java
@@ -2,15 +2,33 @@ package com.DevStream.MoodLogBe.post.domain;
 
 import com.DevStream.MoodLogBe.auth.domain.User;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+
 public class Post {
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String title;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name= "user_id")
     private User author;
     private String content;
     private Boolean autoSaved;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @CreationTimestamp
+    private LocalDateTime updatedAt;
 }
