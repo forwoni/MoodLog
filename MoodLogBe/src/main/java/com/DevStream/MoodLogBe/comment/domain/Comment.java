c7b07bfd593eb75ab30bd4493cd41fefc163f3aa
forwoni <ktion0619@naver.com>
2025-05-21 11:40:04 +0900

    feat(comment): add entity, repository

---
+++ MoodLogBe/src/main/java/com/DevStream/MoodLogBe/comment/domain/Comment.java
@@ -0,0 +1,39 @@
+package com.DevStream.MoodLogBe.comment.domain;
+
+import com.DevStream.MoodLogBe.auth.domain.User;
+import com.DevStream.MoodLogBe.post.domain.Post;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User author;
+
+    @Column(nullable = false)
+    private String content;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
+}
