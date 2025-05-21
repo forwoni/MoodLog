9b61e0369f15c2d662ceb634cfb4dae04732a0b8
forwoni <ktion0619@naver.com>
2025-05-21 16:40:36 +0900

    feat(social): add Repository methods, FollowService

--- MoodLogBe/src/main/java/com/DevStream/MoodLogBe/social/repository/FollowRepository.java
+++ MoodLogBe/src/main/java/com/DevStream/MoodLogBe/social/repository/FollowRepository.java
@@ -5,7 +5,11 @@ import com.DevStream.MoodLogBe.social.domain.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.List;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
+    boolean existsByFollowerAndFollowing(User follower, User following);
     Optional<Follow> findByFollowerAndFollowing(User follower, User following);
+    List<Follow> findByFollower(User follower);
+    List<Follow> findByFollowing(User following);
 }
