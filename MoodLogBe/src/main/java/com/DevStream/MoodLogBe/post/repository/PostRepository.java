26d2e8b729be7320a5565e952fb5d165954a5bdf
Hawon Nam <ktion0619@naver.com>
2025-05-21 17:28:34 +0900

    feat: add post sort (#38)
    * feat(post): implement post list retrieval by user with sorting and pagination
    
    * feat(post): add UserPostController
    
    * feat(post): add sort api end-point

--- MoodLogBe/src/main/java/com/DevStream/MoodLogBe/post/repository/PostRepository.java
+++ MoodLogBe/src/main/java/com/DevStream/MoodLogBe/post/repository/PostRepository.java
@@ -1,7 +1,14 @@
 package com.DevStream.MoodLogBe.post.repository;
 
+import com.DevStream.MoodLogBe.auth.domain.User;
 import com.DevStream.MoodLogBe.post.domain.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+    Page<Post> findByAuthorUsernameOrderByCreatedAtDesc(String username, Pageable pageable);
+    Page<Post> findByAuthorUsernameOrderByLikeCountDesc(String username, Pageable pageable);
+    Page<Post> findByAuthorUsernameOrderByCommentCountDesc(String username, Pageable pageable);
+    Page<Post> findByAuthor(User author, Pageable pageable);
 }
