3ebaafb4d476d6487d3600c4e6a6bad280d18a48
Hawon Nam <ktion0619@naver.com>
2025-05-23 00:53:37 +0900

    feat : search api (#44)
    * feat(search) : add Search API
    
    * feat(sec): add permit

--- MoodLogBe/src/main/java/com/DevStream/MoodLogBe/post/dto/PostResponseDto.java
+++ MoodLogBe/src/main/java/com/DevStream/MoodLogBe/post/dto/PostResponseDto.java
@@ -1,6 +1,7 @@
 package com.DevStream.MoodLogBe.post.dto;
 
 import com.DevStream.MoodLogBe.comment.dto.CommentResponseDto;
+import com.DevStream.MoodLogBe.post.domain.Post;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -16,4 +17,24 @@ public record PostResponseDto(
         int viewCount,
         int likeCount,
         List<CommentResponseDto> comments
-) {}
+) {
+    public static PostResponseDto from(Post post) {
+        return new PostResponseDto(
+                post.getId(),
+                post.getTitle(),
+                post.getContent(),
+                post.getAutoSaved(),
+                post.getAuthor().getUsername(),
+                post.getCreatedAt(),
+                post.getUpdatedAt(),
+                post.getViewCount(),
+                post.getLikeCount(),
+                post.getComments().stream().map(comment -> new CommentResponseDto(
+                        comment.getId(),
+                        comment.getContent(),
+                        comment.getAuthor().getUsername(),
+                        comment.getCreatedAt()
+                )).toList()
+        );
+    }
+}
